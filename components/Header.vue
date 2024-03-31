363c5023257d784803e2c11fbc9ee5bc34f2e0b3
Louis F <72155852+itsmelouis@users.noreply.github.com>
2024-03-31 05:23:04 +0200

    feat: ajout du composant header

---
+++ components/Header.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="flex flex-row justify-between items-center py-5 px-8 sticky border-b border-black rounded-b-xl">
+    <p class="font-bold text-xl">ERP Stock <span class="italic text-sm font-normal">par Louis et Théo</span></p>
+    <div class="flex flex-row gap-5 items-center">
+      <p class="font-semibold">Admin</p>
+      <img src="/stefan-one.png" alt="Avatar" class="rounded-full w-16 h-16 border border-black">
+    </div>
+  </div>
+</template>
+
