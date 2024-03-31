d7e3f595fd81b072942392dea4c60182eed08b09
Louis F <72155852+itsmelouis@users.noreply.github.com>
2024-03-31 05:22:34 +0200

    chore: ajout des modules nuxt-headlessui et nuxt-icon

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,9 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
+  devtools: { enabled: false },
+  modules: ['@nuxtjs/tailwindcss', 'nuxt-icon', 'nuxt-headlessui'],
+
+  headlessui: {
+    prefix: 'H'
+}
 })
